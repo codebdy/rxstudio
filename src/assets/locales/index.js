f7973bf41241c1b500fa38dbd2cbeb948d3143ef
Water.Li <11011968@qq.com>
2020-03-05 10:37:38 +0800

    VUE实现Studio管理后台（三）：支持多语言国际化（vue-i18n）

--- src/assets/locales/index.js
+++ src/assets/locales/index.js
@@ -1,26 +1,37 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
+//引入Vue-i18n
 Vue.use(VueI18n)
 
-const DEFAULT_LANG = 'zh'
-
+//引入语言资源文件
 const locales = {
   zh: require('./zh.json'),
   en: require('./en.json'),
 }
 
+//构建vuei18n实例，后面会export 这个实例
 const i18n = new VueI18n({
-  locale: DEFAULT_LANG,
+  locale: getDefaultLang(),
   messages: locales,
 })
 
-export const setup = lang => {
-  if (lang === undefined) {
-    lang = navigator.language || navigator.userLanguage
-    console.log(lang)
-    lang = lang.substr(0, 2)
-  }
+//获取浏览器语言环境，截取lang的前2位字符，是因为浏览器语言返回值可能是：
+//zh-cn Chinese(PRC) 
+//zh-tw Chinese(Taiwan Region)
+//zh-hk Chinese(Hong Kong SAR, PRC) 
+//zh-sg Chinese(Singapore) 
+//en-us English(United States) 
+//en English
+function getDefaultLang(){
+  let lang = navigator.language || navigator.userLanguage
+  lang = lang.substr(0, 2)
+  return lang
+}
+
+//进行一些基础配置
+export const setup = () => {
+  let lang = getDefaultLang()
 
   Object.keys(locales).forEach(lang => {
     document.body.classList.remove(`lang-${lang}`)
