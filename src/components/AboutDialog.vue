195aea353c8cc5663576319803550a4951c23312
Water.Li <11011968@qq.com>
2020-03-09 21:30:08 +0800

    VUE实现Studio管理后台（十四）：完结

--- src/components/AboutDialog.vue
+++ src/components/AboutDialog.vue
@@ -17,7 +17,8 @@
       基于VUE实现，代码已转入RXeditor项目。<br />
       本原型不再维护，仅供学习参考。<br />
       RXEditor是一个开源的，可视化的，HTML编辑工具，基于Bootstrap实现。<br />
-      RXEditor 代码地址：<br/><a href="https://github.com/vularsoft/rxeditor" target="_blank">https://github.com/vularsoft/rxeditor</a>
+      RXEditor 代码地址：<a href="https://github.com/vularsoft/rxeditor" target="_blank">https://github.com/vularsoft/rxeditor</a>
+      演示地址：<a href="https://vular.cn/rxeditor/" target="_blank" >https://vular.cn/rxeditor</a>
     </div>
     <div class="dialog-footer">
       <div class="dialog-button confirm-btn"
@@ -54,9 +55,10 @@ export default {
 .about-content{
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items:flex-start;
   font-size:14px;
-  line-height: 34px;
+  line-height: 32px;
+  padding-left: 40px;
 }
 
 .about-content a{
